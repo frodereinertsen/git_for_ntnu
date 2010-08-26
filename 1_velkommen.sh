5bb6d54b97d0255f920bba08d143cf0b4b436a00
Frode Reinertsen <frode.reinertsen@gmail.com>
2010-08-26 13:19:54 +0200

    Initial work

---
+++ 1_velkommen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PS1=": "
+clear
+echo "Velkommen til en introduksjon til distribuert versjonskontroll med Git og GitHub!"
+
+for fn in *; do
+  agenda_number=${fn:0:1}
+  alias $agenda_number=". ./$fn"
+done
