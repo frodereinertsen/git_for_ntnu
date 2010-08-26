fa842b5e34440e2e24260cb034de03df0b3c69c2
Frode Reinertsen <frode.reinertsen@gmail.com>
2010-08-26 15:24:00 +0200

    don't know

--- agenda_1_agenda.sh
+++ agenda_1_agenda.sh
@@ -4,8 +4,8 @@ clear
 echo " --- Agenda --- "
 echo ""
 for fn in `ls agenda*.sh`; do
-  substring_length=$((${#fn}-12))
-  substring_name=${fn:9:substring_length}
+  substring_length=$((${#fn}-10))
+  substring_name=${fn:7:substring_length}
   name_without_underscore=${substring_name//_/ }
   echo "$name_without_underscore"
 done
--- agenda_2_distribuert_versjonskontroll.sh
+++ agenda_2_distribuert_versjonskontroll.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 PS1=""
+
 clear
+
+write(){
+for i in `seq 1 5`; do
+  echo ""
+done
+echo "  $1"
+read WAIT
+clear
+}
+
+write "du kan være både tjener og klient"
+write "heisan"
+
 
