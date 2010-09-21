dfd0f9eef9f5c7559fa3ec3b37511af329d17af5
frodereinertsen <frode.reinertsen@gmail.com>
2010-09-21 11:52:17 +0200

    tidy

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,16 +2,16 @@
 source scripts/include.sh
 
 set_prompt ""
-newline
+newline 3
+clear
 title "Distribuert versjonskontroll med"
 super_title "Git og GitHub"
-newline `expr $ROWS - 20`
+newline `expr $ROWS - 18`
 line
 say_right "\"Someday, and that day may never come, I'll call upon you to do a service for me.\""
 newline
 say_right "Don Corleone"
 line
-newline
 
 alias init=". scripts/init.sh"
 alias agenda=". scripts/agenda.sh"
